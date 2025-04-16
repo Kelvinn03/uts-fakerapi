63f3c1d6c8e6ba6a174c6ce22b73bfc4b9863c72
p0ice <67363618+zxhiend@users.noreply.github.com>
2025-04-16 20:27:50 +0700

    updated routes.js to include creditcards endpoint

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const books = require('./components/books/books-route');
 const users = require('./components/users/users-route');
+const creditcards = require('./components/creditCards/creditcards-route');
 
 module.exports = () => {
   const app = express.Router();
 
   books(app);
   users(app);
+  creditcards(app);
 
   return app;
 };
