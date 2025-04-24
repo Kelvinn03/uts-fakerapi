90d890103c2573f7c01b8c359bbed864ecb3752a
Shinichi Wijaya <wijayashinichi@gmail.com>
2025-04-24 20:56:38 +0700

    Update quotes-schema.js

--- src/models/quotes-schema.js
+++ src/models/quotes-schema.js
@@ -1,13 +1,9 @@
-const { Collection } = require("mongoose");
-
 module.exports = (db) =>
   db.model(
-    'Quote',
+    'Quotes',
     db.Schema({
       content: String,
       author: String,
       category: String,
-    }, {
-      Collection: 'quotes'
     })
   );
