882b1e5f260fa9ad426fc6606876f77d37458626
Shinichi Wijaya <wijayashinichi@gmail.com>
2025-04-25 00:17:56 +0700

    Update texts-service.js

--- src/api/components/texts/texts-service.js
+++ src/api/components/texts/texts-service.js
@@ -2,14 +2,11 @@ const textsRepository = require('./texts-repository');
 const getRandomIds = require('../../../utils/randomizer');
 
 async function getTexts(quantity, seed, locale, characters) {
-  console.log('[Debug] Mengambil data dari DB...');
-  const allTexts = await textsRepository.getTexts();
-  console.log('[Debug] Data dari DB:', allTexts);
-  let texts = await textsRepository.getTexts(locale);
+  let texts = await textsRepository.getTexts();
   if (characters) {
-    texts = texts.filter(text =>
-      text.content.length <= parseInt(characters, 10)
-    )
+    texts = texts.filter(
+      (text) => text.content.length <= parseInt(characters, 10)
+    );
   }
   const ids = texts.map((_, index) => index);
   const randomIds = getRandomIds(seed, ids);
