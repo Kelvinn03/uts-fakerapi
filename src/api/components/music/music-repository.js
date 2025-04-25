607ac9a66aef708c3e48c614df54162b67f72f2f
Andronikus G. Pangabdi <andronikus.535240166@stu.untar.ac.id>
2025-04-25 18:20:29 +0700

    Update on repository file

--- src/api/components/music/music-repository.js
+++ src/api/components/music/music-repository.js
@@ -3,7 +3,11 @@ const { Music } = require('../../../models');
 async function getMusic(locale) {
   const query = { _locale: locale };
 
-  return Music.find(query).select('album artist genre index song_name');
+  let musics = Music.find(query).select('album artist genre index song_name');
+  if (musics.length === 0) {
+    musics = Music.find().select('album artist genre index song_name');
+  }
+  return Music;
 }
 
 module.exports = {
--- src/api/components/people/people-repository.js
+++ src/api/components/people/people-repository.js
@@ -3,11 +3,16 @@ const { People } = require('../../../models');
 async function getPeople(locale) {
   const query = { _locale: locale };
 
-  return People.find(query).select(
+  let peoples = People.find(query).select(
     'firstname lastname email phone birthday gender address'
   );
+  if (peoples.length === 0) {
+    peoples = People.find().select(
+      'firstname lastname email phone birthday gender address'
+    );
+  }
+  return People;
 }
-
 module.exports = {
   getPeople,
 };
